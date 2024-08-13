7b9e703fe95d4a504c771d93399389927e28a05c
Elif Vural <elifvural019@gmail.com>
2024-08-13 14:02:07 +0300

    loop commits

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -7,6 +7,12 @@ namespace CSharpStudying
         static void Main(string[] args)
         {
             Console.WriteLine("hello loops");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
 
     }
