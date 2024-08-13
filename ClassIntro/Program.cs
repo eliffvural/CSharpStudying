aa81103cb10f4e09b87ed72aa83568dd8e1c8eea
Elif Vural <elifvural019@gmail.com>
2024-08-13 15:33:55 +0300

    class commit

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -10,7 +10,22 @@ namespace ClassIntro
             string name = "emre";
             int yas = 22;
 
-            Course course1;
+            Course course1= new Course();
+            course1.CourseName = "C#";
+            course1.Teacher = "Emre Taş";
+            course1.Watching = 658000;
+
+            Course course2 = new Course();
+            course2.CourseName = "java";
+            course2.Teacher = "Elif Taş";
+            course2.Watching = 802300;
+
+            Course course3 = new Course();
+            course3.CourseName = "cpp";
+            course3.Teacher = "Hijjj Taş";
+            course3.Watching = 236500;
+
+            Console.WriteLine(course1.CourseName);
 
             Console.WriteLine("hello class");
 
@@ -21,10 +36,8 @@ namespace ClassIntro
     {
         public string CourseName { get; set; }
         public string Teacher { get; set; }
-        public string Watching { get; set; }
+        public int Watching { get; set; }
        
-
-
 
     }
 
