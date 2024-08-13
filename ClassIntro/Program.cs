5574878e4d898d09a9ca77f0fc2549bed1f88135
Elif Vural <elifvural019@gmail.com>
2024-08-13 15:27:02 +0300

    prop commit

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -6,9 +6,26 @@ namespace ClassIntro
     {
         static void Main(string[] args)
         {
+
+            string name = "emre";
+            int yas = 22;
+
+            Course course1;
+
             Console.WriteLine("hello class");
 
         }
+    }
+
+    class Course
+    {
+        public string CourseName { get; set; }
+        public string Teacher { get; set; }
+        public string Watching { get; set; }
+       
+
+
+
     }
 
 }
