87a4260a74ee4dc1997bc1836ea262c24992d546
Elif Vural <elifvural019@gmail.com>
2024-08-13 13:26:48 +0300

    first commit

--- CSharpStudying/Program.cs
+++ CSharpStudying/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace CSharpStudying
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 20000;
+            double faizOrani = 1.15;
+            bool sistemeGirisYapmisMi = false;
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+          
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+        }
+    }
+}
