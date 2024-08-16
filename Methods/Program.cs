cf8fb9e2aa65565e2674b88369acfd5daec80cad
Elif Vural <elifvural019@gmail.com>
2024-08-16 19:50:40 +0300

    product2 commits for class

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -15,17 +15,23 @@ namespace Methods
             //lets create a class like this;
 
             Product product1 = new Product();
+            Product product2 = new Product();
 
             product1.ProductName = "apple";
             product1.Id = 1;
             product1.Description = "apple apple apple";
             product1.Price = 50;
 
+            product2.ProductName = "orange";
+            product2.Id = 2;
+            product2.Description = "orange orange orange";
+            product2.Price = 60;
+
             //lets create array;
 
             Product[] products = new Product[] { product1};
 
-            //foreach
+            //foreach 
 
             foreach (Product product in products)
             {
