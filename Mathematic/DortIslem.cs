f94c8e8e9e73eba47ec41da02ec2f43911fea056
Elif Vural <elifvural019@gmail.com>
2024-08-16 20:54:03 +0300

    committed toplam

--- Mathematic/DortIslem.cs
+++ Mathematic/DortIslem.cs
@@ -12,6 +12,7 @@ namespace Mathematic
         public void Topla(int number1, int number2) 
         {
                int toplam= number1 + number2;
+               Console.WriteLine(toplam);
         
         }
     }
