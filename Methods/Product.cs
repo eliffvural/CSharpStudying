f67c4622e549e323738ec082b163fd59c9f03b4d
Elif Vural <elifvural019@gmail.com>
2024-08-16 14:54:05 +0300

    product description committed

--- Methods/Product.cs
+++ Methods/Product.cs
@@ -11,6 +11,7 @@ namespace Methods
         public int Id{ get; set; }
         public string ProductName { get; set; }
         public int Price { get; set; }
+        public string Description { get; set; }
 
     }
 }
