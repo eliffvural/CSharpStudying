7bec779252f4a93e9a1d6f3348a83914125c7c29
Elif Vural <elifvural019@gmail.com>
2024-08-18 16:51:41 +0300

    different way to add

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -14,7 +14,11 @@ namespace OOP1
             product1.CategoryId = 2;
             product1.ProductName = "Table";
             product1.UnitPrice = 500;
-            product1.UnitsInStock=3;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id=2, CategoryId=2, ProductName="television", UnitPrice=60000, UnitsInStock=4};
+
+
 
 
         }
