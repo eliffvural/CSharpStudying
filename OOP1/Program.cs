a03bc81603726f9946c3a31dc29326ef546deb87
Elif Vural <elifvural019@gmail.com>
2024-08-18 16:26:38 +0300

    cerating oop1

---
+++ OOP1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello oop1!");
+
+        }
+    }
+
+}
