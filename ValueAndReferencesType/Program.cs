2e89c0aea394a4482a1c9bdc9b8daac9c9f6eabb
Elif Vural <elifvural019@gmail.com>
2024-08-18 13:34:00 +0300

    values and references types created

---
+++ ValueAndReferencesType/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ValueAndReferencesType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello types");
+        }
+    }
+
+}
