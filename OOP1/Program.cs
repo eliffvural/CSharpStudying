fe073b6d605548a0516b3da37e4b8d407ef160cf
Elif Vural <elifvural019@gmail.com>
2024-08-18 16:48:11 +0300

    added product's features

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -10,6 +10,9 @@ namespace OOP1
             //Console.WriteLine("hello oop1!");
 
             Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
 
         }
     }
