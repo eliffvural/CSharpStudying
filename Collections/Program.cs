2f6ea7989e32a9c05b584197641c13e4955978a5
Elif Vural <elifvural019@gmail.com>
2024-08-20 00:50:15 +0300

    created list

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -14,12 +15,16 @@ namespace Collections
             Console.WriteLine(nouns[0]);
             Console.WriteLine(nouns[1]);
             Console.WriteLine(nouns[2]);
-            Console.WriteLine(nouns[3]);
-            Console.WriteLine(nouns[4]);
+            //Console.WriteLine(nouns[3]);
+            //Console.WriteLine(nouns[4]);
 
+            //new ile birlikte yeni referans adresi tanimladik ve eski elemanlari kaybetmis oluruz
             nouns = new string[5];
             nouns[4] = "minikk hijjj";
+            Console.WriteLine(nouns[4]);
+            Console.WriteLine(nouns[0]);
 
+            List<string> nouns2 = new List<string> { };
 
 
 
